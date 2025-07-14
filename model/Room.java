ff09cd193ad567f9871f9d5d489fae4e770897f3
adamtfa <adam.tuffaha@outlook.de>
2025-07-14 20:40:11 +0200

    Finale Abgabe

--- model/Room.java
+++ model/Room.java
@@ -11,9 +11,6 @@ import java.io.Serializable;
 
 public class Room implements Serializable {
 
-    // Bitte serialVersionUID beibehalten, damit die Klasse bei der
-    // Speicherung als Datei (Serialisierung) und beim Laden (Deserialisierung)
-    // konsistent bleibt und Versionierungsprobleme vermieden werden. 
     private static final long serialVersionUID = 3947234501234725063L;
 
     private final int number;
